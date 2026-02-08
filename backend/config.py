5c185161ea7b41ec11a8dc8904a5665c64cfbe95
noahapursell <noahapursell@gmail.com>
2026-02-07 23:59:03 -0600

    wip audio

--- backend/config.py
+++ backend/config.py
@@ -9,5 +9,6 @@ class Config(BaseModel):
     db_name = os.environ.get("MONGO_DB")
     embed_dim = int(os.environ.get("EMBED_DIM")) # type: ignore
     embedding_model = "text-embedding-3-small"
+    assembly_ai_api_key: str = os.environ["ASSEMBLY_AI_API_KEY"]
 
 config = Config()
