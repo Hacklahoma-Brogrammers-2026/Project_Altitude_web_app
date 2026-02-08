7bdacec2168b7fdd69dfc4f41d58e6c1ca3f5e66
Robert_Miller <robert.a.miller.410@gmail.com>
2026-02-08 07:02:28 -0600

    lol

--- backend/services/audio_embedding_service.py
+++ backend/services/audio_embedding_service.py
@@ -1,21 +1,21 @@
-# import os
-# # os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"  # quick workaround if needed
+import os
+# os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"  # quick workaround if needed
 
-# import numpy as np
-# from speechbrain.inference.speaker import SpeakerRecognition
-# from speechbrain.dataio import audio_io  # type: ignore
+import numpy as np
+from speechbrain.inference.speaker import SpeakerRecognition
+from speechbrain.dataio import audio_io  # type: ignore
 
-# AUDIO_FILE_DIR = None
+AUDIO_FILE_DIR = None
 
-# verifier: SpeakerRecognition = SpeakerRecognition.from_hparams(
-#     source="speechbrain/spkrec-ecapa-voxceleb",
-#     savedir="pretrained_models/spkrec-ecapa",
-# )  # type: ignore
+verifier: SpeakerRecognition = SpeakerRecognition.from_hparams(
+    source="speechbrain/spkrec-ecapa-voxceleb",
+    savedir="pretrained_models/spkrec-ecapa",
+)  # type: ignore
 
-# def init_audio_service():
-#     global AUDIO_FILE_DIR
-#     os.makedirs("data/audio_samples", exist_ok=True)
-#     AUDIO_FILE_DIR = "data/audio_samples"
+def init_audio_service():
+    global AUDIO_FILE_DIR
+    os.makedirs("data/audio_samples", exist_ok=True)
+    AUDIO_FILE_DIR = "data/audio_samples"
 
 
 # def are_audio_samples_from_same_speaker(path1: str, path2: str) -> bool:
@@ -24,6 +24,6 @@
 #     score, prediction = verifier.verify_batch(signal1, signal2)
 #     return prediction.squeeze(0)[0].item()
 
-# if __name__ == "__main__":
-#     print("Running")
-#     print(are_audio_samples_from_same_speaker("./data/noah_audio_sample.wav", "./data/noah_audio_sample.wav"))
+if __name__ == "__main__":
+    print("Running")
+    print(are_audio_samples_from_same_speaker("./data/noah_audio_sample.wav", "./data/noah_audio_sample.wav"))
